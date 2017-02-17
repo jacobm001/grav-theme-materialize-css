dad2cbf7837f57491407f6bf9a9a454faed4e2a8
Jacob Mastel <jacob@mastel.org>
2017-02-16 17:11:14 -0800

    Added a basic rep total for the stackexchange card

---
+++ js/stackexchange.js
@@ -0,0 +1,12 @@
+$.get("https://api.stackexchange.com/2.2/users/1163298/associated", function(data) {
+	rep = 0;
+	
+	for( i = 0; i < data.items.length; i++ ) {
+		if( data.items[i].reputation > 200 ) {
+			rep += data.items[i].reputation;
+		}
+	}
+
+	rep = (rep + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+	$("#reputation").text(rep);
+});
