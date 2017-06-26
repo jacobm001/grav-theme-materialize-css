af4e62ff4e7238b422ab4815fb2e7d819060ab07
Jacob Mastel <jacobm001@gmail.com>
2017-06-26 16:38:45 -0700

    condensed to a single document ready function

--- js/site.js
+++ js/site.js
@@ -1,15 +1,8 @@
-(function($){
-	$(function(){
-
-		$('.button-collapse').sideNav();
-		$('.datepicker').pickadate({
-			selectMonths: true,
-			selectYears: 2
-		});
-
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
-
 $(document).ready(function() {
     $('.container').find('select').material_select();
+    $('.button-collapse').sideNav();
+	$('.datepicker').pickadate({
+		selectMonths: true,
+		selectYears: 2
+	});
 });
